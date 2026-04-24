b0bd228757145ff70140b276c23cddbd2a068e66
Dmitry <dmitry@users.noreply.github.com>
2026-04-24 13:02:53 +0300

    Updates

--- _next/static/ZZMtTGPiufzbfpl3Br2K8/_buildManifest.js
+++ _next/static/ZZMtTGPiufzbfpl3Br2K8/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mimimi.market//_next/:path+",
+        "destination": "/mimimi.market/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
